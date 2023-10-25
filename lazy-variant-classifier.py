4fe183791abea81a8dd640f2f38f276be0a778b6
Enrico R <enricorrx@gmail.com>
2023-10-25 13:29:53 +0200

    refactor

--- lazy-variant-classifier.py
+++ lazy-variant-classifier.py
@@ -13,13 +13,27 @@ data = pd.read_csv(data_file, low_memory=False,
                    false_values=["False"],  # no inferred dtype
                    index_col=0  # first column as index
                    )
-y = data["phenotype"].values
-X = data.drop(columns=["phenotype"]).values
 print("Done.")
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5, random_state=123)
-clf = LazyClassifier(verbose=5, ignore_warnings=True, custom_metric=None)
+print(data.describe())
+print(data.dtypes)
 
+
+label_name = "phenotype"
+train_frac = .5
+point = round(len(data) * train_frac)
+
+# shuffle
+data = data.sample(frac=1.0, random_state=42)
+
+# split
+X_train, y_train = data.iloc[:point].drop(label_name, axis=1), data.iloc[:point][[label_name]]
+X_test, y_test = data.iloc[point:].drop(label_name, axis=1), data.iloc[point:][[label_name]]
+
+# X_train, y_train = X_train.to_numpy(), y_train.to_numpy()
+# X_test, y_test = X_test.to_numpy(), y_test.to_numpy()
+
+clf = LazyClassifier(verbose=5, ignore_warnings=False, custom_metric=None)
 print()
 
 print("Train...", flush=True)
