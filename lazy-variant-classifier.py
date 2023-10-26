72d1c3f474d92d528e96683aabd9dbaea29f6154
Enrico R <enricorrx@gmail.com>
2023-10-26 16:29:08 +0200

    changing to binary int vector fixes lazypredict

--- lazy-variant-classifier.py
+++ lazy-variant-classifier.py
@@ -1,23 +1,37 @@
 import lazypredict
 import pandas as pd
 from lazypredict.Supervised import LazyClassifier
-from sklearn.model_selection import train_test_split
+
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.naive_bayes import BernoulliNB, GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
 
 data_file = "main.csv"
 
 print(f"Using LazyPredict v{lazypredict.__version__}", flush=True)
 
 print("Reading dataset...", flush=True)
+
+
+def conv(v):
+    if v == "True":
+        return 1
+    else:
+        return 0
+
+
+features = pd.read_csv(data_file, nrows=0).columns[1:]
 data = pd.read_csv(data_file, low_memory=False,
-                   true_values=["True"],  # no inferred dtype
-                   false_values=["False"],  # no inferred dtype
+                   # true_values=["True"],  # no inferred dtype
+                   # false_values=["False"],  # no inferred dtype
+                   converters={c: conv for c in features},
                    index_col=0  # first column as index
                    )
 print("Done.")
-
-print(data.describe())
-print(data.dtypes)
-
 
 label_name = "phenotype"
 train_frac = .5
@@ -30,10 +44,9 @@ data = data.sample(frac=1.0, random_state=42)
 X_train, y_train = data.iloc[:point].drop(label_name, axis=1), data.iloc[:point][[label_name]]
 X_test, y_test = data.iloc[point:].drop(label_name, axis=1), data.iloc[point:][[label_name]]
 
-# X_train, y_train = X_train.to_numpy(), y_train.to_numpy()
-# X_test, y_test = X_test.to_numpy(), y_test.to_numpy()
+classifiers = [LinearSVC, Perceptron, RandomForestClassifier, QuadraticDiscriminantAnalysis, KNeighborsClassifier, BernoulliNB, GaussianNB, DecisionTreeClassifier]
+clf = LazyClassifier(verbose=5, ignore_warnings=True, custom_metric=None, classifiers=classifiers)
 
-clf = LazyClassifier(verbose=5, ignore_warnings=False, custom_metric=None)
 print()
 
 print("Train...", flush=True)
