111ee86fdf23e9a210fd38f5bc6f4b25b6f71eb4
Enrico R <enricorrx@gmail.com>
2023-12-13 15:53:41 +0100

    group weight and gain per region batch

--- batch-regions.sh
+++ batch-regions.sh
@@ -4,6 +4,6 @@ source /nfsd/bcb/bcbg/rossigno/.miniconda3/bin/activate
 for dir in exact-shuffle-noestop-eta2-*; do
   (cd $dir
   echo "Analyzing model $dir"
-  python features-regions.py
+  python ../features-regions.py
   )
 done
