7ab62326770e3befce0c5615199de2eea97a04a1
Enrico R <enricorrx@gmail.com>
2024-03-08 01:38:25 +0100

    compute dataset stats

--- dataset-stats.py
+++ dataset-stats.py
@@ -1,11 +1,19 @@
 import pandas as pd
 
 data_file = "datasets/include-chr3/main-integers.csv"
+new_labels_file = "datasets/download/EBV.txt"
+
 print("Reading the datasets...")
 data = pd.read_csv(data_file, low_memory=False,
                    index_col=0,  # first column as index
                    header=0  # first row as header
                    )
+
+new_label = pd.read_csv(new_labels_file, low_memory=False,
+                        index_col=0,  # first column as index
+                        header=0  # first row as header
+                        )
+
 print("Done.")
 
 data.head()
@@ -17,7 +25,7 @@ for j in range(data.shape[1]):
     if j % 100000 == 0:
         print(f"Columns read: {j}")
 
-    first = data.ilo[0, j]
+    first = data.iloc[0, j]
     is_constant_col = True
     for i in range(1, data.shape[0]):
         if first != data.iloc[i, j]:
@@ -29,3 +37,13 @@ for j in range(data.shape[1]):
         constant_cols.append(data.columns[j])
 
 pd.DataFrame(constant_cols).to_csv("constant_columns.csv")
+
+labels = data[["phenotype", "cluster"]]
+labels["ebv"] = new_label["ebv"]
+data.drop(columns=["phenotype", "cluster"])
+
+print("Writing labels to HDF5...")
+labels.to_hdf("dataset.hdf5", key="labels")
+
+print("Writing features to HDF5...")
+data.to_hdf("datasets.hdf5", key="features")
