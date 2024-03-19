3dd94e8c834788845e017cf930218405718f668d
Enrico R <enricorrx@gmail.com>
2024-03-19 01:59:59 +0100

    major reformat

--- xgbclassifier.py
+++ xgbclassifier.py
@@ -33,6 +33,71 @@ def read_cluster_file(cluster_file):
     return clusters
 
 
+def group_by_chromosome(weigths, gains):
+    keys = weigths.keys()
+    counts_group = [0] * 24
+    weigths_group = [0] * 24
+    gains_group = [0] * 24
+    for key in keys:
+        chromosome = int(key[13:15]) - 1  # e.g. CAJNNU010000001.1:4119343
+        counts_group[chromosome] += 1
+        weigths_group[chromosome] += weigths[key]
+        gains_group[chromosome] += gains[key]
+
+    return counts_group, weigths_group, gains_group
+
+
+def group_by_region(weigths, gains):
+    # List of CSV file paths
+    regions_folder = "/nfsd/bcb/bcbg/rossigno/PNRR/variant-classifier/datasets/exclude-chr3/regions/"
+    regions_files = ["Br_mock_broad.csv", "Br_NNV_broad.csv", "Hk_mock_broad.csv", "Hk_NNV_broad.csv", "broad.csv",
+                     "Br_mock_narr.csv", "Br_NNV_narr.csv", "Hk_mock_narr.csv", "Hk_NNV_narr.csv", "narrow.csv"]
+    data_ensemble_file = "/nfsd/bcb/bcbg/rossigno/PNRR/variant-classifier/datasets/exclude-chr3/data_ensemble.csv"
+
+    # Create a list to store lists of strings from each CSV file
+    total_gain = gains.loc[:, 1].sum()
+
+    total_counts = weigths.loc[:, 1].sum()
+    features = set(weigths.index)
+
+    # Read from each CSV file and append the list to the main list
+    print("Peaks\tCounts\tWeights\tGains")
+    for file in regions_files:
+        current_list = pd.read_csv(regions_folder + file).iloc[:, 0].tolist()
+        common_strings = features.intersection(current_list)
+
+        grouped_gains = gains.loc[list(common_strings), 1]
+        grouped_counts = weigths.loc[list(common_strings), 1]
+        print(f"{file}\t"
+              f"{len(common_strings) / len(features) * 100 : .2f} %\t"
+              f"{grouped_gains.sum() / total_gain * 100 : .2f} %\t"
+              f"{grouped_counts.sum() / total_counts * 100 : .2f}%")
+        if file == "broad.csv":
+            print()
+
+def data_ensemble(features, gains):
+    data_ensemble_file = "/nfsd/bcb/bcbg/rossigno/PNRR/variant-classifier/datasets/exclude-chr3/data_ensemble.csv"
+    features = set(features)
+
+    data_ensemble = pd.read_csv(data_ensemble_file, index_col=0, header=0)
+    ensemble_features = list(features.intersection(data_ensemble.index.values))
+    data_ensemble = data_ensemble.loc[ensemble_features, :]
+    data_ensemble["gain"] = gains.loc[ensemble_features, :]
+    data_ensemble.sort_values(by="gain")
+    data_ensemble.to_csv("ensemble.csv")
+
+    print(f"#features in the ensemble: {len(ensemble_features)}")
+
+    if len(ensemble_features) > 0:
+        info_funct = data_ensemble["funct"].value_counts()
+        info_n_tissue = data_ensemble["n_tissue"].value_counts()
+
+        print(info_funct)
+        print(info_n_tissue)
+    else:
+        print("No features in the data ensemble!!!")
+
+
 class XGBoostVariant:
     bst: Booster
     num_trees: int
@@ -46,12 +111,20 @@ class XGBoostVariant:
     y_pred: ndarray
     y_test: ndarray
 
+    target: str
     features = None
-    importance_counts = None
-    importance_gain = None
+    importance_weights = None
+    importance_gains = None
 
-    def __init__(self, model_name, target, num_trees, max_depth, eta, early_stopping,
-                 sample_bytree, method, objective):
+    def __init__(self, model_name, num_trees, max_depth, eta, early_stopping,
+                 sample_bytree, method, objective,
+                 data_file, target_file, validation=False, do_shuffle_features=False,
+                 selected_features_file=None, cluster_file=None, invc=False
+                 ):
+        self.auc = None
+        self.f1 = None
+        self.accuracy = None
+        self.num_features = None
         self.max_depth = max_depth
         self.eta = eta
         self.early_stopping = early_stopping
@@ -59,7 +132,6 @@ class XGBoostVariant:
         self.random_state = 42
         self.model_name = model_name
         self.num_trees = num_trees
-        self.target = target
         self.train_frac = .8
         self.method = method
         self.objective = objective  # binary:logistic or reg:logistic
@@ -67,17 +139,28 @@ class XGBoostVariant:
         if early_stopping is None:
             self.early_stopping = self.num_trees
 
+        self.data_file = data_file
+        self.target_file = target_file
+        self.validation = validation
+        self.do_shuffle_features = do_shuffle_features
+        self.selected_features_file = selected_features_file
+        self.cluster_file = cluster_file
+        self.invc = invc
+
         print(f"Using XGBoost version {xgb.__version__}")
 
-    def read_datasets(self, dataset_dir, validation=False, do_shuffle_features=False, selected_features_file="", cluster_file="", invc=False):
-        start_t = time.time()
-        features_file = dataset_dir + "features.csv"
-        target_file = dataset_dir + f"{self.target}.csv"
-        selected_features_file = dataset_dir + selected_features_file
-        cluster_file = dataset_dir + cluster_file
+    def read_datasets(self, ):
+        data_file = self.data_file
+        target_file = self.target_file
+        validation = self.validation
+        do_shuffle_features = self.do_shuffle_features
+        selected_features_file = self.selected_features_file
+        cluster_file = self.cluster_file
+        invc = self.invc
 
+        start_t = time.time()
         print("Loading features...", flush=True)
-        data = pd.read_csv(features_file, low_memory=False,
+        data = pd.read_csv(data_file, low_memory=False,
                            index_col=0,  # first column as index
                            header=0  # first row as header
                            ).astype(np.int8)
@@ -91,13 +174,17 @@ class XGBoostVariant:
 
         if do_shuffle_features:
             print("Shuffling features...", flush=True)
+            start_shuffle_t = time.time()
             data = data.sample(frac=1, axis=1, random_state=self.random_state)
-            print("Done.", flush=True)
+            stop_shuffle_t = time.time()
+            print(f"Done in {stop_shuffle_t - start_shuffle_t} s", flush=True)
 
         self.features = list(data.columns)
 
         print("Reading targets...", flush=True)
         labels = pd.read_csv(target_file, header=0, index_col=0)
+        self.target = labels.columns[0]
+        print(f"Target is {self.target}")
         print("Done.", flush=True)
 
         print("Splitting the datasets...", flush=True)
@@ -139,14 +226,14 @@ class XGBoostVariant:
 
         print("Stats (train data):")
         print_stats(X_train, y_train, self.target)
-        print("Transforming X_train into DMatrices...")
+        print("Transforming X_train and y_train into DMatrices...")
         self.dtrain = xgb.DMatrix(X_train, y_train)
         print()
 
         if validation:
             print("Stats (validation data):")
             print_stats(X_validation, y_validation, self.target)
-            print("Transforming X_validation into DMatrices...")
+            print("Transforming X_validation and y_validation into DMatrices...")
             self.dvalidation = xgb.DMatrix(X_validation, y_validation)
             print()
         else:
@@ -172,7 +259,7 @@ class XGBoostVariant:
             self.dtrain.set_info(feature_weights=feature_weights)
         """
         if weights is None:
-            weights = self.importance_counts
+            weights = self.importance_weights
 
         fw = []
         for feature in self.features:
@@ -216,8 +303,8 @@ class XGBoostVariant:
         self.best_score = self.bst.best_score
 
         # features importance
-        self.importance_counts = self.bst.get_score(importance_type="weight")
-        self.importance_gain = self.bst.get_score(importance_type="gain")
+        self.importance_weights = self.bst.get_score(importance_type="weight")
+        self.importance_gains = self.bst.get_score(importance_type="gain")
 
         # save model
         self.bst.save_model(f"{self.model_name}.json")
@@ -248,29 +335,52 @@ class XGBoostVariant:
         print(f"FN={false_neg}\tTP={true_pos}")
 
         # accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
-        accuracy = accuracy_score(self.y_test, self.y_pred)
-        f1 = f1_score(self.y_test, self.y_pred)
-        auc = roc_auc_score(self.y_test, self.y_pred)
+        self.accuracy = accuracy_score(self.y_test, self.y_pred)
+        self.f1 = f1_score(self.y_test, self.y_pred)
+        self.auc = roc_auc_score(self.y_test, self.y_pred)
 
-        print(f"Accuracy = {accuracy * 100 : .3f} %")
-        print(f"f1 = {f1 * 100 : .3f} %")
-        print(f"ROC_AUC = {auc * 100 : .3f} %")
+        print(f"Accuracy = {self.accuracy * 100 : .3f} %")
+        print(f"f1 = {self.f1 * 100 : .3f} %")
+        print(f"ROC_AUC = {self.auc * 100 : .3f} %")
 
-        num_feat = 100
-        importance = sorted(self.importance_counts.items(), key=lambda item: item[1], reverse=True)
-        print(f"Top {num_feat}/{len(importance)} features:")
-        print(importance[:num_feat])
+        print_num_feat = 100
+        importance = sorted(self.importance_weights.items(), key=lambda item: item[1], reverse=True)
+        self.num_features = len(importance)
+        print(f"Top {print_num_feat}/{self.num_features} features:")
+        print(importance[:print_num_feat])
 
-    def print_stats1(self):
-        print(f"method name\t{self.model_name}")
-        print(f"algorithm\t{self.method}")
-        print(f"training set\t{self.train_frac*100}%")
-        print(f"validation set\t?")
-        print(f"feature shuffle\t?")
-        print(f"feature sampling\t?")
-        print(f"feature peaks\t?")
-        print(f"early stopping\t?")
-        # TODO complete for sheet!
+    def write_stats(self, stats_file="stats.csv"):
+        with open(stats_file, 'w') as stats:
+            stats.write(f"method name,{self.model_name}\n")
+            stats.write(f"algorithm,{self.method}\n")
+            stats.write(f"training set,{self.train_frac * 100}%\n")
+            stats.write(f"validation set,{self.validation}\n")
+            stats.write(f"feature shuffle,{self.do_shuffle_features}\n")
+            stats.write(f"feature sampling,{self.by_tree}\n")
+            stats.write(f"feature peaks,{self.selected_features_file}\n")
+            stats.write(f"early stopping,{self.early_stopping}\n")
+            stats.write(f"#trees,{self.num_trees}\n")
+            stats.write(f"eta,{self.eta}\n")
+            stats.write(f"max depth,{self.max_depth}\n")
+
+            stats.write("\n")
+
+            stats.write(f"#features,{self.num_features}\n")
+            stats.write(f"f1,{self.f1}\n")
+            stats.write(f"ROC AUC,{self.auc}\n")
+            stats.write(f"best iteration,{self.best_it}\n")
+            stats.write(f"#tree created,{self.bst.num_boosted_rounds()}\n")
+
+            stats.write("\n")
+
+            stats.write("CHR,Count,Weight,Gain\n")
+            counts, weights, gains = group_by_chromosome(self.importance_weights, self.importance_gains)
+            for i in range(24):
+                stats.write(f"{i + 1},{counts[i]},{weights[i]},{gains[i]}\n")
+
+            stats.write("\n")
+
+            # TODO complete for sheet!
 
     def plot_trees(self, tree_set=None, tree_name=None):
         print("Printing trees...")
@@ -288,46 +398,20 @@ class XGBoostVariant:
 
     def write_importance(self, filename):
         with open(filename + ".counts.csv", 'w') as importance_file:
-            for item in self.importance_counts.items():
+            for item in self.importance_weights.items():
                 importance_file.write(f"{item[0]}, {item[1]}\n")
 
         with open(filename + ".gains.csv", 'w') as importance_file:
-            for item in self.importance_gain.items():
+            for item in self.importance_gains.items():
                 importance_file.write(f"{item[0]}, {item[1]}\n")
-
-    def region_distribution(self):
-        narrow_file = "narrow.csv"
-        narrow_mock_hk_file = "Hk_mock_narr.csv"
-        narrow_mock_br_file = "Br_mock_narr.csv"
-        narrow_nvv_hk_file = "Hk_mock_narr.csv"
-        narrow_nvv_br_file = "Br_NNV_narr.csv"
-
-        broad_file = "broad.csv"
-        broad_mock_hk_file = "Hk_mock_broad.csv"
-        broad_mock_br_file = "Br_mock_broad.csv"
-        broad_nvv_hk_file = "Hk_mock_broad.csv"
-        broad_nvv_br_file = "Br_NNV_broad.csv"
-
-        all_narrow_file = [narrow_file, narrow_nvv_br_file, narrow_nvv_hk_file, narrow_mock_br_file,
-                           narrow_mock_hk_file]
-        all_broad_file = [broad_file, broad_nvv_br_file, broad_nvv_hk_file, broad_mock_br_file, broad_mock_hk_file]
-
-        def read_and_intersect(file_path, features: set):
-            df = pd.read_csv(file_path)
-            lst = df.iloc[:, 0].tolist()
-            return len(features.intersection(lst))
-
-        s = set(self.importance_counts.keys())
-        for b in all_broad_file + all_narrow_file:
-            num = read_and_intersect(b, s)
-            print(f"{b}: {num} ({num / len(s) * 100} %)")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='XGBoost variant classifier')
     parser.add_argument("--model_name", type=str, default="default-model", help="Model name")
-    parser.add_argument("--dataset", type=str, default="/nfsd/bcb/bcbg/rossigno/PNRR/variant-classifier/datasets/", help="Features csv file")
-    parser.add_argument("--target", type=str, default="mortality", help="Target csv file")
+    parser.add_argument("--dataset", type=str, default="features.csv",
+                        help="Features csv file")
+    parser.add_argument("--target", type=str, default="mortality.csv", help="Target csv file")
     parser.add_argument("--select", type=str, default=None, help="List of feature to select")
     parser.add_argument("--cluster", type=str, default=None, help="List of cluster points for test/train")
     parser.add_argument('--invc', default=False, action="store_true",
@@ -348,9 +432,15 @@ if __name__ == "__main__":
 
     print(args)
 
-    clf = XGBoostVariant(model_name=args.model_name, target=args.target, num_trees=args.num_trees, max_depth=args.max_depth, eta=args.eta,
-                         sample_bytree=args.sample_bytree, method=args.method, early_stopping=args.early_stopping, objective=args.objective)
-    clf.read_datasets(args.dataset, validation=args.validate, do_shuffle_features=args.shuffle_features, selected_features_file=args.select, cluster_file=args.cluster, invc=args.invc)
+    clf = XGBoostVariant(model_name=args.model_name, num_trees=args.num_trees,
+                         max_depth=args.max_depth, eta=args.eta,
+                         sample_bytree=args.sample_bytree, method=args.method, early_stopping=args.early_stopping,
+                         objective=args.objective,
+                         data_file=args.dataset, target_file=args.target, validation=args.validate,
+                         do_shuffle_features=args.shuffle_features,
+                         selected_features_file=args.select, cluster_file=args.cluster, invc=args.invc
+                         )
+    clf.read_datasets()
 
     try:
         os.mkdir(args.model_name)
@@ -367,9 +457,11 @@ if __name__ == "__main__":
         clf.set_weights(equal_weight=True)  # for next iteration
 
     clf.plot_trees(tree_name="weighted")
+    clf.write_stats()
 
 # TODO add variable constraints
 # TODO add features sampling by node, level
 # TODO add data points sampling
 # TODO add scale_pos_weight to balance classes
 # TODO add gamma (high for conservative algorithm)
+# TODO add random forest (parallel trees)
